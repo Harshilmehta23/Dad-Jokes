158b48c48f1c78d137e47e38aeb4be68f6d57117
Harshilmehta23 <mharshil60@gmail.com>
2021-02-01 14:49:21 +0530

    Implemented localStorage, advance css and get 10 new random jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Joke from './Joke';
+import { v4 as uuidv4 } from 'uuid';
 import './JokeList.css';
 
 export class JokeList extends Component {
@@ -9,20 +11,59 @@ export class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: [],
+      jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
+      loading: false,
     };
+    this.handleClick = this.handleClick.bind(this);
   }
-  async componentDidMount() {
+  componentDidMount() {
+    if (this.state.jokes.length === 0) this.getJokes();
+  }
+
+  async getJokes() {
     let jokes = [];
     while (jokes.length < this.props.numJokesToGet) {
       let res = await axios.get('https://icanhazdadjoke.com/', {
         headers: { Accept: 'application/JSON' },
       });
-      jokes.push(res.data.joke);
+      jokes.push({ id: uuidv4(), text: res.data.joke, votes: 0 });
     }
-    this.setState({ jokes: jokes });
+    this.setState(
+      (st) => ({
+        loading: false,
+        jokes: [...st.jokes, ...jokes],
+      }),
+      () =>
+        window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    );
+    window.localStorage.setItem('jokes', JSON.stringify(jokes));
   }
+
+  handleVote(id, delta) {
+    this.setState(
+      (st) => ({
+        jokes: st.jokes.map((j) =>
+          j.id === id ? { ...j, votes: j.votes + delta } : j
+        ),
+      }),
+      () =>
+        window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    );
+  }
+
+  handleClick() {
+    this.setState({ loading: true }, this.getJokes);
+  }
+
   render() {
+    if (this.state.loading) {
+      return (
+        <div className='JokeList-spinner'>
+          <i className='far fa-8x fa-laugh fa-spin'></i>
+          <h1 className='JokeList-title'>Loading...</h1>
+        </div>
+      );
+    }
     return (
       <div className='JokeList'>
         <div className='JokeList-sidebar'>
@@ -33,12 +74,20 @@ export class JokeList extends Component {
             src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'
             alt='Smiling Face'
           />
-          <button className='JokeList-getmore'>New Jokes</button>
+          <button className='JokeList-getmore' onClick={this.handleClick}>
+            New Jokes
+          </button>
         </div>
 
         <div className='JokeList-jokes'>
           {this.state.jokes.map((j) => (
-            <div>{j}</div>
+            <Joke
+              key={j.id}
+              votes={j.votes}
+              text={j.text}
+              upvote={() => this.handleVote(j.id, 1)}
+              downvote={() => this.handleVote(j.id, -1)}
+            />
           ))}
         </div>
       </div>
